dab3f7087730d1052facafec4c57ba497a5fd98a
Akshay Mathur <akshaymathur242@gmail.com>
2024-12-19 17:06:05 +0530

    create

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css'
 
 const App = () => {
   const [users, setUsers] = useState([]);
-  // console.log(users);
+  const [user,setUser]=useState({
+    name:'',age:''
+  })
+  
+  console.log(user);
   
 
   useEffect(() => {
@@ -17,17 +21,61 @@ const App = () => {
       console.error('Error fetching users:', error);
     } else {
       setUsers(data);
-    }
-    // const data = await supabase
-    //            .from('users')
-    //            .select('*');
-    //            setUsers(data)
-    //            console.log(data);
-               
+    }             
   }
 
+  function handleChange(e) {
+      setUser(prevFormData=>{
+        return{
+          ...prevFormData,
+        [ e.target.name]:e.target.value,
+        }
+      }) 
+  }
+
+  async function AddUser() {
+    await supabase
+           .from('users')
+           .insert({name:user.name,age:user.age})
+  }
   return (
     <div className="p-4">
+  
+
+    {/* Add User Form */}
+    <form onSubmit={AddUser} className="mb-4">
+      <div className="flex flex-col gap-4 mb-4">
+        <input
+          type="text"
+          placeholder="Name"
+          name = 'name'
+          // onChange={(e) => setName(e.target.value)}
+          onChange={handleChange}
+          className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          name='age'
+          // value={age}
+          // onChange={(e) => setAge(e.target.value)}
+          onChange={handleChange}
+          className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+        />
+
+        <button
+        type="submit"
+        className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+        >
+        Add User
+      </button>
+      </div>
+      
+    </form>
+
+    {/* {message && <p className="mb-4 text-sm text-center text-gray-700">{message}</p>} */}
+
+
     <h1 className="text-2xl font-semibold mb-4">User Table</h1>
     <div className="overflow-x-auto">
       <table className="min-w-full border-collapse border border-gray-200 shadow-md rounded-lg">
